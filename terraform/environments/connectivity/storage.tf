3a6a5093eea217a6afcab999130839a577d06c09
rupinder-j-singh <rupinder.jeet@gmail.com>
2026-07-06 15:25:08 +0100

    fix: add UK West diagnostics storage, fix flow log regions, add connection monitor dependencies

--- terraform/environments/connectivity/storage.tf
+++ terraform/environments/connectivity/storage.tf
@@ -31,3 +31,23 @@ resource "azurerm_storage_account" "diagnostics" {
     application = "diagnostics"
   })
 }
+resource "azurerm_storage_account" "diagnostics_ukw" {
+  name                            = "strsrsconndiagukwp01"
+  resource_group_name             = azurerm_resource_group.diagnostics.name
+  location                        = "ukwest"
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+
+  blob_properties {
+    versioning_enabled = true
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  tags = merge(var.tags, {
+    application = "diagnostics"
+  })
+}
